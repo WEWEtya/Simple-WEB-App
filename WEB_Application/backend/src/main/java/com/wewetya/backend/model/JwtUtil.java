305db0e3a6c5801744332739006dfb11ee252c16
WEWEtya <mistersaoman@gmail.com>
2025-05-22 14:12:51 +0200

    Add Prettier as a development dependency and create package-lock.json

---
+++ WEB_Application/backend/src/main/java/com/wewetya/backend/model/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.wewetya.backend.model;
+
+import java.sql.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+    private final String secret = "secret_key";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
